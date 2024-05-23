43a6c7b5f3f0d95a79d9c577db3c99378f15ee35
Hardik Grover <hardikgrover141@gmail.com>
2024-05-23 23:58:42 +0530

    Final

--- VacayApp/src/components/GitHub/Github.jsx
+++ VacayApp/src/components/GitHub/Github.jsx
@@ -5,8 +5,8 @@ function Github() {
 const data = useLoaderData()
 
   return (
-    <div>Github Followers: {data.followers}
-      <img src={data.avatar_url} alt="Github Picture" width={300} />
+    <div className='text-5xl text-red-800 flex justify-center items-center  text-center font-semibold mt-10 mb-10'>Github Followers: {data.followers}
+      <img className='mt-10 ml-20' src={data.avatar_url} alt="Github Picture" width={300} />
     </div>
   )
 
--- VacayApp/src/components/User/User.jsx
+++ VacayApp/src/components/User/User.jsx
@@ -4,7 +4,10 @@ import {useParams} from 'react-router-dom'
 function User() {
     const {userId} = useParams()
   return (
-    <div>User: {userId}</div>
+    <div>
+    <div className='text-5xl text-red-800 text-center font-semibold mt-[200px] mb-10' >User: {userId}</div>
+    <p className='text-2xl text-black text-center font-medium mb-[200px]'>Welcome to the travelling duniya!!</p>
+    </div>
   )
 }
 
