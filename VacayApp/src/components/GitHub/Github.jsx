80ce00e6f88659dcc86614929f70eb9bd76b5c1c
Hardik Grover <hardikgrover141@gmail.com>
2024-05-24 00:14:22 +0530

    final

--- VacayApp/src/components/GitHub/Github.jsx
+++ VacayApp/src/components/GitHub/Github.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { useLoaderData } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 function Github() {
 const data = useLoaderData()
 
   return (
-    <div className='text-5xl text-red-800 flex justify-center items-center  text-center font-semibold mt-10 mb-10'>Github Followers: {data.followers}
+    <div className='text-5xl text-white flex justify-center items-center text-center font-extrabold mt-10 mb-10'>
+      <Link to="https://github.com/hardikg25" className="bg-red-800 p-4 rounded-lg hover:text-white hover:bg-black">
+            Visit My GitHub
+      </Link>
       <img className='mt-10 ml-20' src={data.avatar_url} alt="Github Picture" width={300} />
     </div>
   )
