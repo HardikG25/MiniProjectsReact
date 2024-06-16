ac0342ac972f1de2a5772ac21bcbcbd731b4cdb0
Hardik Grover <hardikgrover141@gmail.com>
2024-06-16 18:39:36 +0530

    Final

--- ToDoApp/src/App.jsx
+++ ToDoApp/src/App.jsx
@@ -37,9 +37,9 @@ function App() {
 
   return (
     <ToDoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleCompleted}}>
-      <div className="bg-[#172842] min-h-screen py-8">
+      <div className="bg-[#390e33] min-h-screen py-8">
           <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
-              <h1 className="text-2xl font-bold text-center mb-8 mt-2">Write & Manage Your Reminders</h1>
+              <h1 className="text-2xl font-bold text-center mb-8 mt-2">Write & Manage Your Thoughts</h1>
               <div className="mb-4">
                   <TodoForm /> 
               </div>
